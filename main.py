04a41da5f0712155aedaa12106f0c39c0a714ee8
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-14 11:15:03 -0400

    added small changes to main.py for deckbuilder

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/deckbuilder')
+def deck_builder():
+	return 'Deck builder page!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
