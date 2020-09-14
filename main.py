3686f57d0263e73396901ee2154febfd85e0ee70
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-14 17:21:05 -0400

    added working button

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from utils import get_dataframe
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
-@app.route('/deckbuilder')
+@app.route('/deckbuilder', methods=['GET','POST'])
 def deck_builder():
-	return get_dataframe()
+	if request.method == 'POST':
+		if request.form.get('Home'):
+			return redirect('/')
+	elif request.method == 'GET':
+		return render_template('deckbuilder.html', data=get_dataframe())
 
 if __name__ == '__main__':
 	app.run(debug=True)
