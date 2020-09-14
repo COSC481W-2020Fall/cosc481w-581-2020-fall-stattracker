346291c45d1c5bc991459f56295fcfda9ac9b721
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-14 13:53:09 -0400

    moved data into card_data folder

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@ import json
 import pandas as pd
 
 def get_dataframe():
-	data = pd.read_json('set1-en_us.json')
-	data = data[['name', 'region', 'attack', 'cost', 'health', 'rarity']]
+	data = pd.read_json('card_data/set1-en_us.json')
+	data = data[['name', 'region', 'attack', 'health']]
 	return data.to_html()
 
 if __name__ == '__main__':
-	data = pd.read_json('set1-en_us.json')
+	data = pd.read_json('card_data/set1-en_us.json')
 	print(data.head())
 	print(data.columns)
