d2caa104a5ac4a9b3dab169e53f0128378ae2505
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-15 14:06:34 -0400

    added ability to add cards to deck and save decks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from utils import get_dataframe
+import pandas as pd
+import glob
 
 app = Flask(__name__)
 
@@ -9,11 +11,42 @@ def hello_world():
 
 @app.route('/deckbuilder', methods=['GET','POST'])
 def deck_builder():
-	if request.method == 'POST':
-		if request.form.get('Home'):
+	dataFrame = get_dataframe()
+	html_code = dataFrame.to_html()
+	decks = glob.glob('decks/*.csv')
+
+	if request.method == 'POST': ## Gets some input from page
+
+		if request.form.get('Home'): ## Redirect to home page
 			return redirect('/')
-	elif request.method == 'GET':
-		return render_template('deckbuilder.html', data=get_dataframe())
+
+		if request.form.get('deckName'): ## Create empty csv file for deck info
+			deckName = request.form['deckName']
+
+			## Save deck and add to list of available decks
+			deck = pd.DataFrame()
+			path = f'decks/{deckName}.csv'
+			deck.to_csv(path)
+			decks.append(path)
+
+		if request.form.get('cardID'): ## Receive card id to add to deck
+			cardID = request.form['cardID']
+			row = dataFrame.loc[dataFrame['cardCode'] == cardID]
+
+			## Write row to specified deck database
+			path = f'decks/mydeck.csv'
+			deck = pd.read_csv(path)
+			deck = deck.append(row, ignore_index=True)
+			deck.to_csv(path, index=False)
+
+		return render_template(
+			'deckbuilder.html',
+			data=html_code,
+			deck=deck.to_html(),
+			decks=decks)
+
+	elif request.method == 'GET': ## If initial reqeust of page, display it
+		return render_template('deckbuilder.html', data=html_code, decks=decks)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def get_dataframe():
 	data = pd.read_json('card_data/set1-en_us.json')
-	data = data[['cardCode', 'name', 'region', 'attack', 'cost', 'health', 'descriptionRaw', 'keywords']]
-	return data.to_html()
+	data = data[['cardCode', 'name', 'region', 'attack', 'cost', 'health', 'keywords']]
+	return data
 
 if __name__ == '__main__':
 	data = pd.read_json('card_data/set1-en_us.json')
