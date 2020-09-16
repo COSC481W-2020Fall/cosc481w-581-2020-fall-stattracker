c4fc461d7e2b186bb526f5759f3b70f4828077bd
Ryoumi <ddaloisi@emich.edu>
2020-09-16 15:26:44 -0400

    Updated main and templates

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from utils import get_dataframe
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods =["GET","POST"])
+def Home():
+	if request.method == "POST":
+		print("Hello World")
+		if request.form.get("Deck_Builder"):
+			print("This is line 11")
+			return redirect("deckbuilder")
+		if request.form.get("Card_Gallery"):
+			return redirect("gallery")
+	else:
+		return render_template("Home.html") 
+
 
 @app.route('/deckbuilder')
 def deck_builder():
