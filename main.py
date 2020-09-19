d3c331db1facdc0c85f0fff617e0bb010811105b
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-19 18:40:50 -0400

    fixed selection and added deletion

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from utils import get_dataframe
 import pandas as pd
 import glob
+import os
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def hello_world():
 @app.route('/deckbuilder', methods=['GET','POST'])
 def deck_builder():
 	dataFrame = get_dataframe()
-	html_code = dataFrame.to_html()
+	cardList = dataFrame.to_html()
 	decks = glob.glob('decks/*.csv')
 
 	if request.method == 'POST': ## Gets some input from page
@@ -20,39 +21,57 @@ def deck_builder():
 		if request.form.get('Home'): ## Redirect to home page
 			return redirect('/')
 
-		if request.form.get('deckName'): ## Create empty csv file for deck info
+		## Create new deck
+		if request.form.get('deckName'):
 			deckName = request.form['deckName']
-
 			## Save deck and add to list of available decks
-			deck = pd.DataFrame()
+			activeDeck = pd.DataFrame()
 			path = f'decks/{deckName}.csv'
-			deck.to_csv(path)
+			activeDeck.to_csv(path)
 			if path not in decks:
 				decks.append(path)
+			activeDeck = activeDeck.to_html()
 
+		## Select deck to view
+		if request.form.get('selectDeck'):
+			deckName = request.form['selectDeck']
+			activeDeck = pd.read_csv(deckName)
+			activeDeck = activeDeck.to_html()
+
+		## Deletes deck
+		if request.form.get('deleteDeck'):
+			deckName = request.form['deleteDeck']
+			os.remove(deckName)
+			decks.remove(deckName)
+			deckName = None
+			activeDeck = None
+
+		## Add card to deck
 		if request.form.get('cardID'): ## Receive card id to add to deck
-			deckName = 'mydeck'
-			# print(request.form['selectDeck'])
 			cardID = request.form['cardID']
 			row = dataFrame.loc[dataFrame['cardCode'] == cardID]
 
 			## Write row to specified deck database
-			path = f'decks/{deckName}.csv'
-			deck = pd.read_csv(path)
-			deck = deck.append(row, ignore_index=True)
-			deck.to_csv(path, index=False)
+			activeDeck = pd.read_csv(deckName)
+			activeDeck = activeDeck.append(row, ignore_index=True)
+			activeDeck.to_csv(deckName, index=False)
+			activeDeck = activeDeck.to_html()
 
+		## Renders webpage after post request
 		return render_template(
 			'deckbuilder.html',
-			data=html_code,
-			deck=deck.to_html(),
+			deckName=deckName,
+			cardList=cardList,
+			activeDeck=activeDeck,
 			decks=decks)
 
-	elif request.method == 'GET': ## If initial reqeust of page, display it
+	## Initial page request
+	elif request.method == 'GET':
 		return render_template(
 			'deckbuilder.html',
-			data=html_code,
-			deck=None,
+			deckName=None,
+			cardList=cardList,
+			activeDeck=None,
 			decks=decks)
 
 if __name__ == '__main__':
