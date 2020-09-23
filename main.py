5506a7ca8fc52d8cc72dfdd72178d027a0d4eea4
Alex <akim8@emich.edu>
2020-09-22 20:48:26 -0400

    finish card page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from utils import get_dataframe
+from gallery import g_home
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def hello_world():
 @app.route('/deckbuilder')
 def deck_builder():
 	return get_dataframe()
+
+@app.route('/gallery')
+@app.route('/gallery/<cardcode>')
+def gallery(cardcode = None):
+	return g_home(cardcode)
 
 if __name__ == '__main__':
 	app.run(debug=True)
