e29e7de498d1282251f4bffd8bbc281ee1002870
Tristan Shah <123.tristan.shah@gmail.com>
2020-09-27 18:38:29 -0400

    deletes last three ts

--- utils.py
+++ utils.py
@@ -11,16 +11,10 @@ def get_dataframe():
 
 	data = pd.concat(data)
 	data = data[['cardCode', 'name', 'region', 'attack', 'cost', 'health', 'rarity']]
-	func = lambda x: False if 'T' in x[-2:] else True
+	func = lambda x: False if 'T' in x[-3:] else True
 	mask = data['cardCode'].apply(func)
 	data = data[mask]
 	return data
 	
 if __name__ == '__main__':
 	data = get_dataframe()
-
-	func = lambda x: False if 'T' in x[-2:] else True
-	mask = data['cardCode'].apply(func)
-	data = data[mask]
-	print(len(data))
-	print(data)
