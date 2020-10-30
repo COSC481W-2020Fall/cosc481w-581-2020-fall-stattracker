0433f914f5d1fdaedb30055c780dca1971710dad
Tristan Shah <123.tristan.shah@gmail.com>
2020-10-30 18:11:29 -0400

    adding tristan unit test

---
+++ unittesttristan.py
@@ -0,0 +1,33 @@
+import unittest
+from utils import buildFromCode
+from lor_deckcodes import LoRDeck, CardCodeAndCount
+from utils import get_dataframe
+
+class DeckBuilderTests(unittest.TestCase):
+	"""docstring for DeckBuilderTests"""
+
+	def test_buildFromCode(self):
+		deck = get_dataframe()
+		
+		testDeck = deck['cardCode'].value_counts().to_dict()
+		f = lambda key, value: str(value) + ':' + key
+
+		cardList = []
+		for key, value in zip(testDeck.keys(), testDeck.values()):
+			cardList.append(f(key, value))
+
+		testDeck = LoRDeck(cardList)
+		code = testDeck.encode()
+		testDataframe = buildFromCode(code)
+
+		valid = deck['cardCode'].unique().tolist()
+		test = testDataframe['cardCode'].unique().tolist()
+
+		valid.sort()
+		test.sort()
+
+		for x, y in zip(valid, test):
+			self.assertEqual(x, y)
+
+if __name__ == '__main__':
+	unittest.main()
